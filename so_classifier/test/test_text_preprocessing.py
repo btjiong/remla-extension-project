0493d5bd5070ddd5b14e678cb0b15028fd039d37
Bailey Tjiong <btjiong@outlook.com>
2022-05-27 12:01:08 +0200

    Transform the tests into unittests

--- so_classifier/test/test_text_preprocessing.py
+++ so_classifier/test/test_text_preprocessing.py
@@ -1,17 +1,22 @@
 from so_classifier.text_preprocessing import text_prepare
+import unittest
 
 
-def test_text_prepare():
-    examples = ["SQL Server - any equivalent of Excel's CHOOSE function?",
-                "How to free c++ memory vector<int> * arr?"]
-    answers = ["sql server equivalent excels choose function",
-               "free c++ memory vectorint arr"]
-    for ex, ans in zip(examples, answers):
-        if text_prepare(ex) != ans:
-            return "Wrong answer for the case: '%s'" % ex
-    return 'Basic tests are passed.'
+class TestTextPrepare(unittest.TestCase):
+
+    def test_simple_text_prepare(self):
+        examples = ["SQL Server - any equivalent of Excel's CHOOSE function?",
+                    "How to free c++ memory vector<int> * arr?"]
+        answers = ["sql server equivalent excels choose function",
+                   "free c++ memory vectorint arr"]
+        for ex, ans in zip(examples, answers):
+            self.assertEqual(text_prepare(ex), ans)
 
 
 if __name__ == '__main__':
-    print(test_text_prepare())
+    unittest.main()
+
+
+
+
 
--- so_classifier/test/test_text_to_vector.py
+++ so_classifier/test/test_text_to_vector.py
@@ -1,15 +1,17 @@
 from so_classifier.text_to_vector import my_bag_of_words
+import unittest
 
 
-def test_my_bag_of_words():
-    words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
-    examples = ['hi how are you']
-    answers = [[1, 1, 0, 1]]
-    for ex, ans in zip(examples, answers):
-        if (my_bag_of_words(ex, words_to_index, 4) != ans).any():
-            return "Wrong answer for the case: '%s'" % ex
-    return 'Basic tests are passed.'
+class TestTextVector(unittest.TestCase):
+
+    def test_my_bag_of_words(self):
+        words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
+        examples = ['hi how are you']
+        answers = [[1, 1, 0, 1]]
+        for ex, ans in zip(examples, answers):
+            self.assertFalse((my_bag_of_words(ex, words_to_index, 4) != ans).any())
 
 
 if __name__ == '__main__':
-    print(test_my_bag_of_words())
+    unittest.main()
+
