a3bab1d05d97778f3d3390ec73190d78e8bf0fe8
Wessel <oosterbroekwessel@gmail.com>
2022-06-02 11:44:10 +0200

    Reverted failing test

--- test/test_text_to_vector.py
+++ test/test_text_to_vector.py
@@ -8,7 +8,6 @@ class TestTextVector(unittest.TestCase):
         words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
         examples = ['hi how are you']
         answers = [[1, 1, 0, 1]]
-        self.assertFalse(True)
         for ex, ans in zip(examples, answers):
             self.assertFalse((my_bag_of_words(ex, words_to_index, 4) != ans).any())
 
