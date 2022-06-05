095529c62c4192191d574bc0ce6ddc0e2dc3191d
chrisy781 <christiaanwiers@live.nl>
2022-06-05 11:23:33 +0200

    Finished dataVal file

--- so_classifier/data_val.py
+++ so_classifier/data_val.py
@@ -1,45 +1,70 @@
 # put your import statements here
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 
-def read_data():
-    #hier zorgen dat de functie alle 3 path x 2 columns exporteert
-    # train_path = '../data/validation.tsv'
-    # validation_path = '../data/validation.tsv'
-    # test_path = '../data/validation.tsv'
-    # paths = [train_path, validation_path, test_path]
-    # for path in range(len(paths)):
-
-    df = pd.read_csv('../data/validation.tsv', sep='\t')
-    titles = df['title']
-    tags = df['tags']
-    return titles, tags
+paths = ['../data/validation.tsv', '../data/train.tsv', '../data/test.tsv']
 
 
-def check_title():
-    titles, tags = read_data()
-    print('title check')
+def read_data(p):
+    df = pd.read_csv(p, sep='\t')
+    read_data.cols = df.shape[1]
+    print("Amount of columns detected: ", read_data.cols)
+    if (int(read_data.cols)) == 2:
+        titles = df['title']
+        tags = df['tags']
+        if (titles.isnull().sum() & tags.isnull().sum() == 0) and (len(titles) == len(tags)):
+            pass
+        else:
+            print("Missing values are found, please check!")
+        return titles, tags
+
+    elif (int(read_data.cols)) == 1:
+        titles = df['title']
+        if (titles.isnull().sum()) == 0:
+            pass
+        else:
+            print("Missing values are found, please check!")
+        return titles
+
+
+def only_check_title(p):
+    titles = read_data(p)
+    print('Checking the test titles')
+    only_check_title.title_type = titles
+    for i in range(len(only_check_title.title_type)):
+        if (type(only_check_title.title_type[i])) == str:
+            pass
+        else:
+            print(i, only_check_title.title_type[i], 'has is no string and is now deleted')
+            only_check_title.title_type.delete(i)
+    print("checking done")
+    return only_check_title.title_type
+
+
+def check_title(p):
+    titles, tags = read_data(p)
+    print('checking titles')
     check_tags.tags_type = tags
     check_title.title_type = titles
-    for i in range(1, len(check_title.title_type)): #begins at 1 since 0 are column headers title and tags
+    for i in range(len(check_title.title_type)):
         if (type(check_title.title_type[i])) == str:
             pass
         else:
-            print(i, check_title.title_type[i], 'has is no string,and is now deleted')
+            print(i, check_title.title_type[i], 'has is no string and is now deleted')
             check_title.title_type.delete(i)
             check_tags.tags_type.delete(i)
+    print("Checking titles done")
     return check_title.title_type
 
 
-def check_tags():
-    titles, tags = read_data()
+def check_tags(p):
+    titles, tags = read_data(p)
     check_tags.tags_type = tags
     check_title.title_type = titles
-    print('tag check')
-    check_tags.tags_type = tags
-    for i in range(1, len(check_tags.tags_type)): #begins at 1 since 0 are column headers title and tags
-        if (type(check_tags.tags_type[i])) == str: #vgm worden deze tags gezien als strings en niet als list:( !!
+
+    print('Checking tags')
+    for i in range(len(check_tags.tags_type)):
+        if (type(check_tags.tags_type[i])) == str:
             sep_list = ["[", "]", "'"]
 
             for sep in range(len(sep_list)):
@@ -50,43 +75,56 @@ def check_tags():
                     check_tags.tags_type.delete(i)
                     check_title.title_type.delete(i)
         else:
-            print(i, check_tags.tags_type[i], 'tag is no str,and is now deleted')
+            print(i, check_tags.tags_type[i], 'tag is no string and is now deleted')
             check_tags.tags_type.delete(i)
             check_title.title_type.delete(i)
+    print("checking tags done")
     return check_tags.tags_type
 
 
-def save_as_tsv():
-    check_title.title_type = check_title()
-    check_tags.tags_type = check_tags()
+def save_as_tsv(p):
+    print("Now saving")
+    if int(read_data.cols) == 2:
+        check_title.title_type = check_title(p)
+        check_tags.tags_type = check_tags(p)
 
-    new_titles = check_title.title_type
-    new_tags = check_tags.tags_type
-    new_df = new_titles + "," + new_tags
-    print("new csv file")
-    print(new_df)
-    new_df.to_csv('../data/train.tsv', sep='\t')
+        new_titles = check_title.title_type
+        new_tags = check_tags.tags_type
+        new_df = pd.DataFrame({'title': new_titles, 'tags': new_tags})
+
+        new_df.to_csv(p, index=False, sep='\t')
+        print("saved checked .tsv file\n")
+        return
+
+    elif (int(read_data.cols)) == 1:
+        only_check_title.title_type = only_check_title(p)
+        new_titles = only_check_title.title_type
+
+        new_df = pd.DataFrame({'title': new_titles})
+        new_df.to_csv(p, index=False, sep='\t')
+        print("saved checked .tsv test file\n")
+        return
+
+
+def data_validation():
+    print("--- DATA VALIDATION STARTED ---")
+    for x in range(len(paths)):
+        print("NOW CHECKING PATH #", x + 1, paths[x])
+        read_data(paths[x])
+        if (int(read_data.cols)) == 2:
+            check_title(paths[x])
+            check_tags(paths[x])
+            save_as_tsv(paths[x])
+
+        elif (int(read_data.cols)) == 1:
+            only_check_title(paths[x])
+            save_as_tsv(paths[x])
+
+    print("--- CHECKING SUCCESSFULLY FINISHED ---")
     return
 
 
-# def data_validation():
-#     for tsv in range(len(paths)):
-#         check_title(tsv)
-#         check_tags(tsv)
-#         save_as_tsv(path)
-#
-#     return
-
-
-check_title()
-check_tags()
-save_as_tsv()
-
-#--------TO DOs-----------
-#zorgen dat als bij de "title column" een item wordt verwijderd, hetzelfde row item ook bij "tags column" wordt verwijderd
-#en vise versa
-#miss in de function een variabele maken voor de test, train en validation file zodat al die bestand te checken zijn
-
+data_validation()
 
 
 
