6d20786e9cec75a23a9a7e83663eb7ddce612543
Bailey Tjiong <btjiong@outlook.com>
2022-06-06 13:01:36 +0200

    Add port forward to shell script

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -6,3 +6,7 @@ helm repo update
 
 helm install promstack prometheus-community/kube-prometheus-stack
 kubectl apply -f deployment.yml
+
+kubectl port-forward svc/promstack-kube-prometheus-prometheus 9090:9090 &
+
+kubectl port-forward svc/promstack-grafana 3000:80 &
