d0a10d1d14f72dbd6771251e0b667ea5dbe7afc6
Wessel <oosterbroekwessel@gmail.com>
2022-06-06 14:54:11 +0200

    Fixed code quality issues in the sestup and init files

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
+"""
+    Install this python package.
+"""
 import io
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 from so_classifier import __version__
 
-with io.open('requirements.txt', mode="r", encoding="utf-16") as f:
+with io.open("requirements.txt", mode="r", encoding="utf-16") as f:
     required_deps = f.read().splitlines()
 
 setup(
-    name='so-classifier',
+    name="so-classifier",
     version=__version__,
-    description='so_classifier: Tag classifier for StackOverflow titles',
-    url='https://github.com/btjiong/remla-extension-project',
-    author='Péter Angeli, Wessel Oosterbroek, Bailey Tjiong, and Christiaan Wiers',
-    keywords='machine learaning tag inference prediction similarity learning python source code stackoverflow',
+    description="so_classifier: Tag classifier for StackOverflow titles",
+    url="https://github.com/btjiong/remla-extension-project",
+    author="Péter Angeli, Wessel Oosterbroek, Bailey Tjiong, and Christiaan Wiers",
+    keywords=("machine learaning tag inference prediction similarity"
+             "learning python source code stackoverflow"),
     packages=find_packages(),
-    python_requries='>=3.6',
+    python_requries=">=3.6",
     install_requires=required_deps,
     # entry_points={
     #     'console_scripts': [
