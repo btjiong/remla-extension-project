06484aa51bbb1f06b9364e92d238f4574461b349
Bailey Tjiong <btjiong@outlook.com>
2022-06-06 13:10:10 +0200

    Update deploy-cluster.sh

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -8,5 +8,10 @@ helm install promstack prometheus-community/kube-prometheus-stack
 kubectl apply -f deployment.yml
 
 kubectl port-forward svc/promstack-kube-prometheus-prometheus 9090:9090 &
+pid[0]=$!
 
 kubectl port-forward svc/promstack-grafana 3000:80 &
+pid[1]=$!
+
+trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
+wait
